7d27f518a8fb183f670d1635b96a7c9bf58add7a
AaravusingGithub <142517562+AaravusingGithub@users.noreply.github.com>
2026-01-07 20:16:52 +0530

    Enhance Header component with logo and styling
    Added a logo image to the header and wrapped content in a div.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,16 @@
 import Nav from "./Nav";
+import lemonLogo from "../assets/lemon.png";
 
 export default function Header() {
   return (
-    <header>
-      <h1>Little Lemon</h1>
-      <Nav />
+    <header className="site-header">
+      <div className="header-inner">
+        <div className="brand">
+          <img src={lemonLogo} alt="Little Lemon logo" className="logo" />
+          <h1>Little Lemon</h1>
+        </div>
+        <Nav />
+      </div>
     </header>
   );
 }
